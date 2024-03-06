9c844f9c01de07e3b7f59d519509fa7833479712
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-03-05 23:25:54 -0300

    Update exe014.js

--- exercicios/exe014.js
+++ exercicios/exe014.js
@@ -2,13 +2,14 @@ function validateForm() {
     let i = 0;
     let limite = 0;
     let razao = 0
-    limite = parseInt(prompt('Digite o limite da PA'));
-    razao = parseInt(prompt('Digite a razao da PA'));
+    limite = Number(prompt('Digite o limite da PA'));
+    razao = Number(prompt('Digite a razao da PA'));
 
     for (i = 0; i <= limite; i += razao) {
         alert(i);
     }
-    if(limite < razao) {
-        alert('Limite precisa ser maior que razão')
+
+    if (isNaN(limite) || isNaN(razao) || (limite < razao)) {
+        alert('PROGRAMA ENCERRADO!\nTente Novamente')
     }
 }
